57a2261d61af23da87f556d6a18566945f2b422a
sai10rgukt <n170804@rguktn.ac.in>
2022-08-01 07:11:33 +0000

    updated

---
+++ level1_design2/README.md
@@ -0,0 +1,3 @@
+# Final Report - Sai
+## Leve1 Design 2-seq_detect_1011
+adsva dsvj z vmdsv a 
--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -8,6 +8,7 @@ from pathlib import Path
 
 import cocotb
 from cocotb.clock import Clock
+from cocotb.triggers import Timer
 from cocotb.triggers import RisingEdge, FallingEdge
 
 @cocotb.test()
@@ -16,11 +17,48 @@ async def test_seq_bug1(dut):
 
     clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
     cocotb.start_soon(clock.start())        # Start the clock
-
     # reset
     dut.reset.value = 1
     await FallingEdge(dut.clk)  
     dut.reset.value = 0
     await FallingEdge(dut.clk)
-
     cocotb.log.info('#### CTB: Develop your test here! ######')
+    #1
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+    dut._log.info(f"curr_state={dut.current_state.value}")
+    dut._log.info(f"next_state={dut.next_state.value}")
+    dut.current_state.value==dut.SEQ_1.value
+    if dut.next_state !=dut.SEQ_1.value:
+        print("failure at seq_1")
+    #2
+    dut.inp_bit.value=0 
+    await FallingEdge(dut.clk)
+    dut._log.info(f"curr_state={dut.current_state.value}")
+    dut._log.info(f"next_state={dut.next_state.value}")
+    dut.current_state.value==dut.SEQ_10.value
+    if dut.next_state !=dut.IDLE.value:
+        print("failure at seq_10")
+    
+
+    #3
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+    dut.current_state.value==dut.SEQ_101.value
+    dut._log.info(f"curr_state={dut.current_state.value}")
+    dut._log.info(f"next_state={dut.next_state.value}")
+    
+    if dut.next_state !=dut.SEQ_1011.value:
+        print("failure at seq_101")
+    
+    #4
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+    dut._log.info(f"curr_state={dut.current_state.value}")
+    dut._log.info(f"next_state={dut.next_state.value}")
+    dut.current_state.value==dut.SEQ_1011.value
+    if dut.next_state !=dut.SEQ_1.value:
+        print("failure at seq_1011")
+    
+    
+
